8351a2bf3ca99ada5c99073114aa056911ea6fae
Bart Romgens <bart.romgens@gmail.com>
2012-12-15 14:04:24 +0100

    Added comments to serverobserver.

--- src/server/serverobserver.cpp
+++ src/server/serverobserver.cpp
@@ -4,6 +4,7 @@ ServerObserver::ServerObserver()
 {
 }
 
+
 ServerObserver::~ServerObserver()
 {
 }
--- src/server/serverobserver.h
+++ src/server/serverobserver.h
@@ -4,12 +4,27 @@
 #include <string>
 #include <vector>
 
+/**
+ * @brief Observer for Servers, will be notified of new incoming data from clients.
+ */
 class ServerObserver
 {
 public:
+  /**
+   * Constructor.
+   */
   ServerObserver();
+
+  /**
+   * Destructor
+   */
   ~ServerObserver();
 
+  /**
+   * @brief notifyReceivedData
+   * @param dataStrings the list of strings that are send by a client with the connectionId
+   * @param connectionId the connection ID of the connection that sends the message
+   */
   virtual void notifyReceivedData(std::vector<std::string> dataStrings, int id) = 0;
 };
 
